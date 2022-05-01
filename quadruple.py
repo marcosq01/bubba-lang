3978d256dbc88dea913b52a4d5fce24d7d994181
Marcos Quintero <a00824603@itesm.mx>
2022-05-01 02:24:31 -0500

    clase cuadruplo anadida

---
+++ quadruple.py
@@ -0,0 +1,10 @@
+
+
+class Quadruple:
+
+    def __init__(self, op, left, right, result) -> None:
+        self.op = op
+        self.left = left
+        self.right = right
+        self.result = result
+
