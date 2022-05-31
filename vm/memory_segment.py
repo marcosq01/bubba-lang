59e65145edf51b3ab02d08ecc7ab9f85023524a8
Marcos Quintero <42913124+marcosq01@users.noreply.github.com>
2022-05-31 08:59:04 -0500

    valores default en memoria

--- vm/memory_segment.py
+++ vm/memory_segment.py
@@ -1,11 +1,11 @@
 
 class MemorySegment:
     def __init__(self, li, lf, ls, ti=0, tf=0, ts=0) -> None:
-        self.local_int = [None] * li
-        self.local_float = [None] * lf
-        self.local_string = [None] * ls
-        self.temp_int = [None] * ti
-        self.temp_float = [None] * tf
-        self.temp_string = [None] * ts
+        self.local_int = [0] * li
+        self.local_float = [0.0] * lf
+        self.local_string = [""] * ls
+        self.temp_int = [0] * ti
+        self.temp_float = [0.0] * tf
+        self.temp_string = [""] * ts
 
 
